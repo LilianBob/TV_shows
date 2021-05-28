861a8d8051c6f8868af8a5331d23cacd75866457
lilian Robert <mslillianito@gmail.com>
2021-05-27 23:50:53 -0700

    js file added

--- TV_shows_app/models.py
+++ TV_shows_app/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.db.models.fields import CharField
+from django.forms import ModelForm
 
 # Create your models here.
 class Show(models.Model):
     title= models.CharField(max_length=255)
     network= models.CharField(max_length=255)
-    release_date= models.DateField(null=True, blank=True)
+    release_date= models.DateField(blank=True, null=True)
     description= models.TextField()
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at= models.DateTimeField(auto_now=True)
+class ReleaseDate(ModelForm):
+    class Meta:
+        model = Show
+        fields = ['release_date']
---
+++ TV_shows_app/static/app.js
@@ -0,0 +1,9 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+$ (document).ready(function() {
+    console.log("ready!")
+    // $(".datePicker").datepicker({
+    //     changeMonth: true,
+    //     changeYear: true,
+    //     yearRange: "1900:2021",
+    // })
+    })
